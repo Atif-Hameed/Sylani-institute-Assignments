b8f56a208f3802c9da32d2696e434593fa4f9630
Atif <atifhameed11312@gmail.com>
2022-12-30 19:35:27 +0500

    initial commit

---
+++ Assignment_05/assignment_05.js
@@ -0,0 +1,123 @@
+// 01 Find max number btwn 2
+// var num1:number;
+// num1=6;
+// var num2:number;
+// num2=8;
+// var maxNum:number;
+// if(num1>num2){
+//     maxNum=num1;
+// }
+// else
+// maxNum=num2;
+// console.log("The maximum number : "+maxNum);
+//find max number btwn 3
+// var num1:number;
+// num1=90;
+// var num2:number;
+// num2=34;
+// var num3:number;
+// num3=9;
+// var maxNum:number;
+// if(num1>num2 && num1>num3){
+//     maxNum=num1;
+// }
+// else if(num2>num1 && num2>num3){
+//     maxNum=num2;
+// }
+// else {
+//     maxNum=num3;
+// }
+// console.log("The maximum number : "+maxNum);
+//chk  number status
+// var num:number;
+// num= 0;
+// if(num>0){
+//     console.log("Number is positive");
+// }
+// else if(num<0){
+//     console.log("Number is negative");
+// }
+// else{
+//     console.log("Number is Zero");
+// }
+//chk number is divisible
+// var num:number;
+// num= 66;
+// if(num%5==0 && num%11==0){
+//     console.log("Number is divisible by 5 and 11");
+// }
+// else if(num%5==0 && num%11!=0){
+//     console.log("Number is divisible by 5 and not divisible by 11");
+// }
+// else if(num%5!=0 && num%11==0){
+//     console.log("Number is divisible by 11 and not divisible by 5");
+// }
+// else{
+//     console.log("Number is not divisible by 5 neither by 11");
+// }
+// chk evn dd
+// var num:number;
+// num= 0;
+// if(num%2==0){
+//     console.log("Number is Even");
+// }
+// else{
+//     console.log("Number is odd");
+// }
+//chk year
+// var year:number;
+// year= 2024;
+// if(year%4==0){
+//     console.log("Year is Leap");
+// }
+// else{
+//     console.log("Year is not Leap");
+// }
+//check character
+// var character:string;
+// character= "?";
+// if((character>='a' && character<='z') || (character>='A' && character<='Z')){
+//     console.log("character is Alphabet ");
+// }
+// else
+// console.log("character is Not Alhpabet");
+// check vowel or not
+// var char:string|null=prompt("Enter any character : ");
+// if((char=='a'||char=='e'||char=='i'||char=='o'||char=='u')||(char=='A'||char=='E'||char=='I'||char=='O'||char=='U')){
+//     console.log("The Character is a Vowel ");
+// }
+// else{
+//     console.log("Character is not vowel");
+// }
+//check alphabetic
+// var character:any=prompt("Wirte any character : ");
+// if((character>='a'&& character<='z')||(character>='A'&&character<='Z')){
+//     console.log("Character is a Alphabet ")
+// }
+// else if((character>=0 && character<=0)){
+//     console.log("Character is a digit ")
+// }
+// else{
+//     console.log("Character is a special character")
+// }
+//check lower uper case
+// var alpha:any=prompt("Enter any alhpabet");
+// if(alpha>='a'&& alpha<='z'){
+//     console.log("Alphabet is lowercase ");
+// }
+// else if(alpha>='A'&& alpha<='Z'){
+//     console.log("Alphabet is Upercase ");
+// }
+// else{
+//     console.log("Not enter alhpabet :(")
+// }
+// practice program
+var num10 = 10;
+var num13 = 0;
+if (num10 > 0) {
+    num13 = 20;
+}
+else {
+    console.log("exite");
+}
+console.log("Result " + num13);
---
+++ Assignment_06/assignment_06.ts
@@ -0,0 +1,126 @@
+                          //  1-  Natral numbers 1  to  N
+
+/*var n
+n=prompt("Enter the number : ");
+
+var num1=1;
+
+while(num1<=n){
+    console.log(num1)
+    num1++;
+}
+*/
+
+
+                          //  2 -  reverse   N  to   1
+
+/*var n
+n=prompt("Enter the number : ");
+
+var num1=1;
+
+while(num1<=n){
+    console.log(n)
+    n--;
+}
+*/
+
+                           //  3 -  Alphabet   A   to   Z
+
+/*var i=65   //=A
+var j=90//=Z
+
+while(i<=j){
+    console.log(String.fromCharCode(i))
+    i++
+}
+*/
+
+                            //  4 -  All even from 1  to  100
+
+/*var n=1
+while(n<=100){
+    if(n%2==0){//  (n%2!=0)  to print odd
+        console.log(n)
+    }
+    n++
+}
+*/
+
+                            //  5  -  Find sum of number b/w  1  to  n
+
+/*var num=prompt("Enter finit number : ")
+n=1
+
+var sum=0
+
+while(n<=num){
+    sum=sum+n
+    n++
+}
+console.log("Sum of All numbers : ",sum)
+*/
+
+                            //  6  -  Sum of All even numbers
+
+/*var num=prompt("Enter finit number : ")
+n=1
+
+var sum=0
+
+while(n<=num){
+    if(n%2==0){// (n%2!=0) for odd
+        sum=sum+n
+    }
+    n++
+}
+console.log("Sum of All numbers : ",sum)
+*/
+
+                                //  7  -  Multiplication Table of ANy number
+
+/*num=prompt("Write number to find it's table : ")
+num1=Number(num)
+
+limit=10
+n=1
+
+while(n<=limit){
+    console.log(num1,' X ',n,' = ',num1*n)
+    n++;
+}
+*/
+
+                                //  8  -  Find number of digits in a number
+
+/*
+//num1=prompt("Enter any Number : ")
+//num=Number(num1)
+var num:number=10                                
+var n:number=0
+while(num != 0){
+num=Math.floor(num/10)
+n++
+}
+                                
+console.log("number of digits : ",n)
+*/
+                                //  9  -  Find 1st and last digits of a number 
+
+                                
+var num:number=23459
+
+var n1:number=num
+var n2:number
+
+while(n1 >= 10){
+    n1=Math.floor(n1/10)
+}
+n2=num%10
+
+console.log("Fist number : ",n1)
+console.log("Last number : ",n2)
+console.log("Sum of numbers : ",n1+n2)
+
+
+ 
